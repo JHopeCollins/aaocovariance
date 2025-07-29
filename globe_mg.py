8bb13ab860c7e89cb8355e228f80729c97606824
Josh Hope-Collins <joshua.hope-collins13@imperial.ac.uk>
2025-07-29 09:04:12 +0100

    tidy up 2D and sphere scripts

--- globe_mg.py
+++ globe_mg.py
@@ -1,79 +1,112 @@
 from firedrake import *  # noqa: F403
 import asQ
 from utils.mg import icosahedral_mesh
-from utils.mg import ManifoldTransferManager  # noqa: F401
 import numpy as np
 np.random.seed(6)
 
-nt = 8
-coarse_level = 1
-fine_level = 5
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
 
-D = 0.2
-nu = D*D/(2*nt - 4)
-nu_c = Constant(nu)
+parser = ArgumentParser(
+    description="ParaDiag solver for diffusion correlation operator on the unit sphere",
+    formatter_class=ArgumentDefaultsHelpFormatter
+)
+parser.add_argument('--ref_level', type=int, default=3, help='Refinement level of icosahedral sphere. Total number of cells is 20*4^ref_level.')
+parser.add_argument('--base_level', type=int, default=1, help='Refinement level of coarsest grid. Total number of cells is 20*4^ref_level.')
+parser.add_argument('--D', type=float, default=0.2, help='Daley lengthscale.')
+parser.add_argument('--degree', type=int, default=1, help='Degree of the CG finite element space.')
+parser.add_argument('--nslices', type=int, default=1, help='Number of time-slices in the all-at-once system. Must divide the number of MPI ranks exactly.')
+parser.add_argument('--slice_length', type=int, default=8, help='Number of timesteps per time-slice. Total number of timesteps in the all-at-once system is nslices*slice_length.')
+parser.add_argument('--show_args', action='store_true', help='Print all the arguments when the script starts.')
 
-assert nt % COMM_WORLD.size == 0
-time_partition = [nt//COMM_WORLD.size for _ in range(COMM_WORLD.size)]
+args = parser.parse_known_args()
+args = args[0]
+
+if args.show_args:
+    PETSc.Sys.Print(args)
+
+# Set up time parallelism
+time_partition = [args.slice_length for _ in range(args.nslices)]
 ensemble = asQ.create_ensemble(time_partition)
+nt = sum(time_partition)
 
+# Icosahedral sphere mesh:
+# - has multigrid hierarchy, from coarsest mesh with base_refs
+# - coarsest level has base_level refinements
+# - halos (overlap) set up for Vanka or Star patches
 mesh = icosahedral_mesh(
     R0=1, degree=1,
-    base_level=coarse_level,
-    nrefs=fine_level-coarse_level,
+    base_level=args.base_level,
+    nrefs=args.ref_level-args.base_level,
     distribution_parameters={
         "partition": True,
         "overlap_type": (DistributedMeshOverlapType.VERTEX, 2)},
     comm=ensemble.comm
 )
 
-V = FunctionSpace(mesh, "CG", 1)
+V = FunctionSpace(mesh, "CG", args.degree)
+
+# Show number of degrees of freedom
 PETSc.Sys.Print(f"{V.dim() = }")
+
+# Random ICs to hit range of modes
 ics = Function(V)
 ics.dat.data[:] = np.random.random_sample(ics.dat.data.shape)
 
+# diffusion coefficient from correlation lengthscale
+D = args.D
+nu = D*D/(2*nt - 4)
+nu_c = Constant(nu)
 
+
+# mass matrix
 def form_mass(u, v):
     return inner(u, v)*dx
 
 
+# stiffness matrix
 def form_function(u, v, t):
     return inner(nu_c*grad(u), grad(v))*dx
 
 
+# import multigrid transfers for non-nested grids
+from utils.mg import ManifoldTransferManager  # noqa: F401
+
+### Configure the solver
 parameters = {
-    "snes_type": "ksponly",
-    "ksp": {
-        "view": ":logs/ksp_view.log",
-        "converged_rate": None,
-        "monitor": None,
-        "rtol": 1e-10,
+    "snes_type": "ksponly",               # No nonlinear solver
+    "ksp": {                              # Print out some useful information
+        "view": ":logs/ksp_view.log",     # Write linear solver details to file
+        "converged_rate": None,           # Print out convergence rate and iteration count
+        "monitor": None,                  # Print out residual at each iteration
     },
-    "ksp_type": "richardson",
+    "ksp_rtol": 1e-6,                     # Outer relative tolerance
+    "ksp_type": "richardson",             # Fixed point iterations
     "pc_type": "python",
-    "pc_python_type": "asQ.CirculantPC",
-    "circulant_state": "linear",
-    "circulant_alpha": 1e-8,
+    "pc_python_type": "asQ.CirculantPC",  # ParaDiag preconditioner
+    "circulant_state": "linear",          # No need to rebuild the blocks
+    "circulant_alpha": 1e-5,              # Circulant parameter
     "circulant_block": {
-        "ksp_rtol": 1e-6,
-        "ksp_type": "gmres",
-        "pc_type": "mg",
-        "pc_mg_type": "multiplicative",
-        "pc_mg_cycle_type": "v",
+        "ksp_max_it": 100,                # Maximum linear iterations
+        "ksp_converged_maxits": None,     # Don't crash if we hit max iterations
+        "ksp_rtol": 1e-6,                 # Inner relative tolerance
+        "ksp_type": "preonly",            # Single V-cycle application
+        "pc_type": "mg",                  # Multigrid preconditioner
+        "pc_mg_type": "multiplicative",   # Apply levels multiplicatively
+        "pc_mg_cycle_type": "v",          # V-cycles
         "mg_transfer_manager": f"{__name__}.ManifoldTransferManager",
-        "mg_levels": {
-            "ksp_max_it": 3,
-            "ksp_type": "chebyshev",
-            "ksp_chebyshev_esteig": "0,0.25,0,1.05",
+        "mg_levels": {                    # Solver options for each level
+            "ksp_max_it": 5,              # Number of smoothing steps
+            "ksp_type": "chebyshev",      # Chebyshev iterations
+            "ksp_chebyshev_esteig": "0,0.25,0,1.05",  # target top end of spectrum
             "esteig_ksp_max_it": 20,
             "pc_type": "python",
-            "pc_python_type": "firedrake.ASMVankaPC",
-            "pc_vanka": {
+            "pc_python_type": "firedrake.ASMStarPC",  # Additive Schwarz method with "star" patches
+            "pc_star": {
                 "construct_dim": 0,
                 "sub_sub_pc_type": "lu"
             },
         },
-        "mg_coarse": {
+        "mg_coarse": {  # Direct LU solve with MUMPS on coarsest grid
             "pc_type": "python",
             "pc_python_type": "firedrake.AssembledPC",
             "assembled_mat_type": "aij",
@@ -83,12 +116,14 @@ parameters = {
         },
     },
 }
+# Configure options specifically for block j with 'circulant_block_{j}'
 for j in range(nt):
-    prefix = f"circulant_block_{j}"
-    log_prefix = f":logs/block_{j}"
-    parameters[f"{prefix}_ksp_converged_rate"] = f"{log_prefix}_ksp_rate.log"
-    parameters[f"{prefix}_ksp_view"] = f"{log_prefix}_ksp_view.log"
+    parameters[f"circulant_block_{j}"] = {
+        "ksp_converged_rate": f":logs/block_{j}_ksp_rate.log",  # Log convergence rates
+        # "ksp_max_it": j  # Variable iterations per block?
+    }
 
+### Set up the all at once system
 paradiag = asQ.Paradiag(
     ensemble=ensemble,
     time_partition=time_partition,
@@ -99,12 +134,18 @@ paradiag = asQ.Paradiag(
 
 paradiag.solve()
 
-aaos_its = paradiag.linear_iterations + 1  # extra application to precondition rhs
+### Print out some iteration count diagnostics
+
+# Number of outer iterations
+aaos_its = paradiag.linear_iterations
+# Extra preconditioner application to precondition rhs
+pc_its = aaos_its + 1
+
+# Number of inner iterations
 block_its = paradiag.block_iterations.data()
 max_its = max(block_its)
 min_its = min(block_its)
 
-PETSc.Sys.Print(f"Block iterations: {block_its}")
-
-PETSc.Sys.Print(f"Minimum iterations (per block solve): {min_its:>4d} ({min_its/aaos_its:.4e})")
-PETSc.Sys.Print(f"Maximum iterations (per block solve): {max_its:>4d} ({max_its/aaos_its:.4e})")
+PETSc.Sys.Print(f"Outer iterations: {aaos_its:>4d}")
+PETSc.Sys.Print(f"Minimum iterations (per block solve): {min_its:>4d} ({min_its/pc_its:.4e})")
+PETSc.Sys.Print(f"Maximum iterations (per block solve): {max_its:>4d} ({max_its/pc_its:.4e})")
--- tabeart25.py
+++ tabeart25.py
@@ -3,66 +3,74 @@ import asQ
 import numpy as np
 np.random.seed(6)
 
-time_partition = [10]
+nt = 10
+nx = 50
+D = 0.2
+
+# Set up time parallelism
+# Partition of length 1 => time-serial
+time_partition = [nt]
 ensemble = asQ.create_ensemble(time_partition)
-
-nt = sum(time_partition)
-
-nx = 64
-h = 1/nx
 
 mesh = UnitSquareMesh(nx, nx, comm=ensemble.comm)
 
 V = FunctionSpace(mesh, "CG", 1)
+
+# Random ICs to hit range of modes
 ics = Function(V)
 ics.dat.data[:] = np.random.random_sample(ics.dat.data.shape)
 
+# homogeneous boundary conditions
 bcs = [DirichletBC(V, 0, "on_boundary")]
 bcs[0].apply(ics)
 
-D = 0.2
+# diffusion coefficient from correlation lengthscale
 nu = D*D/(2*nt - 4)
 nu_c = Constant(nu)
 
 
+# mass matrix
 def form_mass(u, v):
     return inner(u, v)*dx
 
 
+# stiffness matrix
 def form_function(u, v, t):
     return inner(nu_c*grad(u), grad(v))*dx
 
 
+### Configure the solver
 parameters = {
-    "snes_type": "ksponly",
-    "ksp": {
-        "converged_rate": None,
-        "monitor": None,
-        "rtol": 1e-6,
+    "snes_type": "ksponly",               # No nonlinear solver
+    "ksp": {                              # Print out some useful information
+        "view": ":logs/ksp_view.log",     # Write linear solver details to file
+        "converged_rate": None,           # Print out convergence rate and iteration count
+        "monitor": None,                  # Print out residual at each iteration
     },
-    "ksp_type": "richardson",
+    "ksp_rtol": 1e-6,                     # Outer relative tolerance
+    "ksp_type": "richardson",             # Fixed point iterations
     "pc_type": "python",
-    "pc_python_type": "asQ.CirculantPC",
-    "circulant_state": "linear",
-    "circulant_alpha": 1e-5,
-    "circulant_block": {
-        "ksp_norm_type": "unpreconditioned",
-        "ksp_converged_maxits": None,
-        "ksp_max_it": 500,
-        "ksp_rtol": 1e-6,
-        "ksp_type": "chebyshev",
-        "ksp_chebyshev_esteig": "0.95,0,0,1.05",
-        "esteig_ksp_max_it": 50,
-        "pc_type": "pbjacobi"
+    "pc_python_type": "asQ.CirculantPC",  # ParaDiag preconditioner
+    "circulant_state": "linear",          # No need to rebuild the blocks
+    "circulant_alpha": 1e-5,              # Circulant parameter
+    "circulant_block": {                  # Configure default block solver options
+        "ksp_max_it": 500,                        # Maximum linear iterations
+        "ksp_converged_maxits": None,             # Don't crash if we hit max iterations
+        "ksp_rtol": 1e-6,                         # Inner relative tolerance
+        "ksp_type": "chebyshev",                  # Chebyshev iterations
+        "ksp_chebyshev_esteig": "0.95,0,0,1.05",  # Arnoldi estimate of eigenvalues
+        "esteig_ksp_max_it": 30,                  # Number of Arnoldi iterations
+        "pc_type": "pbjacobi"                     # Point Jacobi preconditioner
     },
 }
+# Configure options specifically for block j with 'circulant_block_{j}'
 for j in range(nt):
-    prefix = f"circulant_block_{j}"
-    log_prefix = f":logs/block_{j}"
-    parameters[f"{prefix}_ksp_converged_rate"] = f"{log_prefix}_ksp_rate.log"
-    parameters[f"{prefix}_ksp_view"] = f"{log_prefix}_ksp_view.log"
-    parameters[f"{prefix}_esteig_ksp_view"] = f"{log_prefix}_esteig_ksp_view.log"
+    parameters[f"circulant_block_{j}"] = {
+        "ksp_converged_rate": f":logs/block_{j}_ksp_rate.log",  # Log convergence rates
+        # "ksp_max_it": j  # Variable iterations per block?
+    }
 
+### Set up the all at once system
 paradiag = asQ.Paradiag(
     ensemble=ensemble,
     time_partition=time_partition,
@@ -73,10 +81,18 @@ paradiag = asQ.Paradiag(
 
 paradiag.solve()
 
-aaos_its = paradiag.linear_iterations + 1  # extra application to precondition rhs
+### Print out some iteration count diagnostics
+
+# Number of outer iterations
+aaos_its = paradiag.linear_iterations
+# Extra preconditioner application to precondition rhs
+pc_its = aaos_its + 1
+
+# Number of inner iterations
 block_its = paradiag.block_iterations.data()
 max_its = max(block_its)
 min_its = min(block_its)
 
-PETSc.Sys.Print(f"Minimum iterations (per block solve): {min_its:>4d} ({min_its/aaos_its:.4e})")
-PETSc.Sys.Print(f"Maximum iterations (per block solve): {max_its:>4d} ({max_its/aaos_its:.4e})")
+PETSc.Sys.Print(f"Outer iterations: {aaos_its:>4d}")
+PETSc.Sys.Print(f"Minimum iterations (per block solve): {min_its:>4d} ({min_its/pc_its:.4e})")
+PETSc.Sys.Print(f"Maximum iterations (per block solve): {max_its:>4d} ({max_its/pc_its:.4e})")
